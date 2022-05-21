22b3df4191561f4265d101e622fba27525596b72
dulanwirajith <dulanwirajith1995@gmail.com>
2022-05-21 23:59:15 +0530

    feat(tasks): add task service

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -2,4 +2,8 @@ import { Injectable } from '@nestjs/common';
 
 // injectable means we can inject this service any component
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  helloWorld() {
+    return 'asd';
+  }
+}
