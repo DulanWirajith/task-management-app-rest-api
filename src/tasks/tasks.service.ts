517168c7060c2f98213705404ab11b54a2e697ae
dulanwirajith <dulanwirajith1995@gmail.com>
2022-05-22 20:48:21 +0530

    feat(tasks): add task model and add types to controller and service

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { Tasks } from './tasks.model';
 
 // injectable means we can inject this service any component
 @Injectable()
 export class TasksService {
-  private tasks: any[] = [];
+  private tasks: Tasks[] = [];
 
-  getAllTasks() {
+  getAllTasks(): Tasks[] {
     return this.tasks;
   }
 }
