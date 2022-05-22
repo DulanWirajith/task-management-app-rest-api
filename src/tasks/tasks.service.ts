a5b6b1dac0c556f64def7a9013d1627c16d317ca
dulanwirajith <dulanwirajith1995@gmail.com>
2022-05-22 19:56:25 +0530

    feat(tasks): change array

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@nestjs/common';
 // injectable means we can inject this service any component
 @Injectable()
 export class TasksService {
-  private tasks: any[] = ['asd', 'asdf'];
+  private tasks: any[] = [];
 
   getAllTasks() {
     return this.tasks;
