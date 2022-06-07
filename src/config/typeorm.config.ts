571d999e976d97a3a9f82114ffd66a0b05a3c8fa
dulanwirajith <dulanwirajith1995@gmail.com>
2022-06-07 17:09:42 +0530

    feat(db-connection): added typeOrmConfig object

---
+++ src/config/typeorm.config.ts
@@ -0,0 +1,14 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+export const typeOrmConfig: TypeOrmModuleOptions = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgress',
+  password: 'DuVlaranjith',
+  database: 'taskmanagement',
+  /* eslint-disable */
+  // entities -> used to translate tables into databse. these are stored in files.  so we schould say to typeorm which files they are
+  entities: [__dirname + '/../**/*.entity.ts'],
+  synchronize: true,
+};
