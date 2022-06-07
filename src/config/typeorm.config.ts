09f4fa9d1b111c516f9ebfa4f25e0cd5dedecd2b
dulanwirajith <dulanwirajith1995@gmail.com>
2022-06-07 18:16:07 +0530

    feat(task-entity): get password from env file

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,11 +1,12 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import 'dotenv/config';
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'postgres',
   host: 'localhost',
   port: 5432,
   username: 'postgres',
-  password: '',
+  password: process.env.password.toString(),
   database: 'taskmanagement',
   /* eslint-disable */
   // entities -> used to translate tables into databse. these are stored in files.  so we schould say to typeorm which files they are
