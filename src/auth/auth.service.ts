180edf9df283279e4c089dccd1b59ce270524d1c
dulanwirajith <dulanwirajith1995@gmail.com>
2022-06-16 16:13:16 +0530

    feat(user-entity): inject user repository. create sign up method

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserRepository } from './user.repository';
+import { UserEntity } from './user.entity';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    @InjectRepository(UserEntity)
+    private readonly userRepository: UserRepository,
+  ) {}
+
+  async signUp(authCredentialDto: AuthCredentialsDto) {
+    const { username, password } = authCredentialDto;
+    const user = new UserEntity();
+    user.username = username;
+    user.password = password;
+    console.log(this.userRepository);
+    await user.save();
+  }
+}
